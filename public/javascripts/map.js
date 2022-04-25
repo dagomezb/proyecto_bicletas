d9970fdb9158f43fc8ca08d2adbd68b8b197416e
Duván Alberto Gómez Betancur <duvan.gomez36@eia.edu.co>
2022-04-25 09:16:43 -0500

    update

---
+++ public/javascripts/map.js
@@ -0,0 +1,3 @@
+var map = L.map('mainmap').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
