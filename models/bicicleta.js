49fcb0449abe3f8be04aae503ca3693a0440526b
Duván Alberto Gómez Betancur <duvan.gomez36@eia.edu.co>
2022-04-26 07:36:23 -0500

    nueva version

---
+++ models/bicicleta.js
@@ -0,0 +1,43 @@
+var bicicleta = function(id, color, modelo, ubicacion){
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+bicicleta.prototype.toString = function(){
+    return 'id: ' + this.id + ' | color: ' + this.color;
+}
+
+bicicleta.allBicis = [];
+
+bicicleta.add = function(aBici){
+    bicicleta.allBicis.push(aBici);
+}
+
+bicicleta.findById = function(aBiciId){
+    var aBici = bicicleta.allBicis.find(x => x.id == aBiciId);
+    if (aBici)
+        return aBici;
+    else
+        throw new Error(`No existe una bicicleta con el id ${aBiciId}`);
+    
+}
+
+bicicleta.removeById = function(aBiciId){
+    //var aBici = bicicleta.findById(aBiciId);
+    for(var i = 0; i< bicicleta.allBicis.length; i++){
+        if(bicicleta.allBicis[i].id == aBiciId){
+            bicicleta.allBicis.splice(i,1);
+            break;
+        }
+    }
+}
+
+var a = new bicicleta(1, 'rojo', 'urbana', [6.231193, -75.601730]);
+var b = new bicicleta(2, 'blanca', 'urbana', [6.238468, -75.602777]);
+
+bicicleta.add(a);
+bicicleta.add(b);
+
+module.exports = bicicleta;
